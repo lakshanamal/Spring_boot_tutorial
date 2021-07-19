228bdf7492d649be67c740aaf649f07c88f12f52
lakshanamal <lakshanamal100@gmail.com>
2021-07-19 13:05:29 +0530

    post requst learned

--- src/main/java/com/example/demo/topic/TopicController.java
+++ src/main/java/com/example/demo/topic/TopicController.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +15,17 @@ public class TopicController {
 	@Autowired
 	private TopicService topicService;
 	
-	@RequestMapping("/topic")
+	@RequestMapping("/topics")
 	public List<Topic> getTopic() {
 		return topicService.getAllTopic();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+	@RequestMapping(method=RequestMethod.POST,value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		 topicService.addTopic(topic);
 	}
 }
--- src/main/java/com/example/demo/topic/TopicService.java
+++ src/main/java/com/example/demo/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.example.demo.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,12 +9,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topic =  Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("1","Lakshan","16 years old"),
 			new Topic("2","Lakshani","17 years old"),
 			new Topic("3","Lakshanika","15 years old")
-			);
+			));
 	public List<Topic> getAllTopic(){
-		return topic;
+		return topics;
+	}
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+	}
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 }
